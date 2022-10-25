71d06cd9691d7deebdcf93594161d66e7da3eb31
Ahad Albaqami <a.albaqami@lean.sa>
2022-10-25 17:35:24 +0300

    Add Firebase Packages

---
+++ HUGPETGITHUB/HUGPETGITHUB/HUGPETGITHUBApp.swift
@@ -0,0 +1,17 @@
+//
+//  HUGPETGITHUBApp.swift
+//  HUGPETGITHUB
+//
+//  Created by Ahad Obaid Albaqami on 29/03/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct HUGPETGITHUBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HUGPETGITHUB/HUGPETGITHUB/Views/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HUGPETGITHUB
+//
+//  Created by Ahad Obaid Albaqami on 29/03/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, Abrar!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
