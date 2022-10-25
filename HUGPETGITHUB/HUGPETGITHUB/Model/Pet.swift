50c0515198cf5aafbb110765c129a3a4919efa94
abrar <llabrrar@gmail.com>
2022-10-25 21:54:56 +0300

    Add services view

---
+++ HUGPETGITHUB/HUGPETGITHUB/Model/Pet.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+
+enum PetTypes:  String, Identifiable, CaseIterable , Codable {
+    
+    public var id: String {
+        return self.rawValue.capitalized
+    }
+    
+    case Dog
+    case Cat
+    case Rabbit
+    case Bird
+}
